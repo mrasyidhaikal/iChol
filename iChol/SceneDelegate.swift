ce7817f8f6482ae3ee446b253d9c0ac7ad40f145
Windy <windywu812@gmail.com>
2020-11-02 10:03:52 +0700

    initial commit

--- iChol/SceneDelegate.swift
+++ iChol/SceneDelegate.swift
@@ -8,21 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         if let windowScene = scene as? UIWindowScene {
-                  let window = UIWindow(windowScene: windowScene)
-
-                   window.rootViewController = ViewController()
-            window.rootViewController = UINavigationController(rootViewController: ViewController())
-                   self.window = window
-                   window.makeKeyAndVisible()
-              }
+            window = UIWindow(windowScene: windowScene)
+            window?.rootViewController = UINavigationController(rootViewController: ViewController())
+            window?.makeKeyAndVisible()
+        }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
